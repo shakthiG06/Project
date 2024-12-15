bddb153e3b84c9b7c1b36e13fa8c2401c40903bf
shakthiG06 <shakthi3176@gmail.com>
2024-12-15 13:19:39 +0530

    Returned html

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world, You are at blog's index")
+
+def detail(request, post_id):
+    return HttpResponse(f"You are viewing the post details. And the ID is {post_id}")
