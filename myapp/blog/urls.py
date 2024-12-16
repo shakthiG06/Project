9d18a0eadb070662bb66c0cfb37aed0e78be4819
shakthiG06 <shakthi3176@gmail.com>
2024-12-16 06:26:31 +0530

    Staic files

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -7,7 +7,5 @@ urlpatterns=[
     path("",views.index,name="index"),
     path("post/<str:post_id>",views.detail,name="detail"),
     path("new_something_url",views.new_url_view,name="new_page_url"),
-    path("old_url",views.old_url_redirect,name="old_url"),
-
-
+    path("old_url",views.old_url_redirect,name="old_url")
 ]
